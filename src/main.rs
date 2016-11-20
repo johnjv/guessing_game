9f5f4616d22b47d92c32ef89ca66d59f8b187e0e
Justin Varghese John <justinjohn107@gmail.com>
2016-11-20 00:54:01 -0800

    Add game loop

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("A GUESSING GAME");
-    println!("please input whatever:");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read input");
+    loop {
+        println!("please input a number between 1 and 100:");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read input");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("input is not a number");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal   => {
+                println!("correct!");
+                break;
+            }
+        }
+    }
     
-    println!("your input: {}", guess);
 }
